60d319915508c0c5a71557ef0a75436a17fe670c
Jhon Andres Parra Rodriguez <ja.parrar12@uniandes.edu.co>
2023-09-10 20:19:49 -0500

    creacion microservicio api gateway

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -2,8 +2,16 @@ from flask_restful import Resource
 from ..modelos import db, Oferta, OfertaSchema, Empresa, EmpresaSchema
 from flask import request
 from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+from celery import Celery
 
-import logging
+celery_app = Celery(__name__, broker="redis://localhost:6379/0")
+
+@celery_app.task(name="registrar_log")
+def registrar_log(*args):
+    pass
+
+import traceback
 
 oferta_schema = OfertaSchema()
 empresa_Schema = EmpresaSchema()
@@ -39,19 +47,20 @@ class VistaOfertasEmpresa(Resource):
             descripcion = request.json.get("descripcion")
             lenguajes = request.json.get("lenguajes")
             empresa = request.json.get("empresa")
-
             nueva_oferta = Oferta(
                 titulo=titulo,
                 descripcion=descripcion,
                 lenguajes=lenguajes,
                 empresa=empresa
             )
-
+            print("error")
+            args = (titulo, datetime.utcnow())
+            registrar_log.apply_async(args=args, queue="logs")
             db.session.add(nueva_oferta)
             db.session.commit()
-
             return oferta_schema.dump(nueva_oferta), 201
 
         except Exception as e:
             db.session.rollback()
+            traceback.print_exc()
             return {"message": "Error al crear la oferta: {}".format(str(e))}, 500  
